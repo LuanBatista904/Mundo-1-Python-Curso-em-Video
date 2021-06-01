33bdc65b4e93013cf62848c9ad660398d2b411d6
LuanBatista904 <luanbatista904@outlook.com>
2021-05-31 20:27:40 -0400

    forth

--- desafio23.py
+++ desafio23.py
@@ -1,2 +1,6 @@
-num = input('Digite um valor entre 1000 e 9999: ')
-print('unidade: {}\nDezena: {}\nCentena: {}\nMilhar:{}'.format(num[3],num[2],num[1],num[0]))
+num = int(input('Digite um valor entre 1000 e 9999: '))
+u =  num // 1 % 10
+d = num // 10 % 10
+c = num //100%10
+m = num //100%10
+print('Unidade: {}\nDezena: {}\nCentena: {}\nMilhar:{}'.format(u, d, c, d))
